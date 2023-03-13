1ed90450c3163252e067d82dd500d7e4462d2afc
Kristian <127381299+KristiyanSt@users.noreply.github.com>
2023-03-13 15:04:07 +0200

    Added new task called "04 Caesar Cipher"

---
+++ 04. Caesar Cipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string text = Console.ReadLine();
+
+StringBuilder cryptedWord = new StringBuilder();
+
+foreach (char ch in text)
+{
+    cryptedWord.Append((char)(ch + 3));
+}
+
+Console.WriteLine(cryptedWord);
