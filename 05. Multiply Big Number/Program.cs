c3ff39975fb8eb90cac06a538be8888d7e53e249
Kristian <127381299+KristiyanSt@users.noreply.github.com>
2023-03-13 14:51:08 +0200

    Adding 05. Multiply Big Number and 07. String Explosion tasks

---
+++ 05. Multiply Big Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+string number = Console.ReadLine();
+int multiplier = int.Parse(Console.ReadLine());
+
+StringBuilder sb = new StringBuilder();
+int remainder = 0;
+if (multiplier != 0)
+{
+    for (int i = number.Length - 1; i >= 0; i--)
+    {
+        int currDigit = int.Parse(number[i].ToString());
+
+        int result = currDigit * multiplier + remainder;
+        remainder = 0;
+
+
+
+        if (i == 0)
+        {
+            sb.Insert(0, result);
+            break;
+        }
+
+        if (result > 9)
+        {
+            sb.Insert(0, result % 10);
+            remainder = result / 10;
+        }
+        else
+        {
+            sb.Insert(0, result);
+        }
+
+    }
+}
+else
+{
+    sb.Insert(0, 0);
+}
+
+Console.WriteLine(sb);
---
+++ 07. String Explosion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+string text = Console.ReadLine();
+
+int strength = 0;
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == '>')
+    {
+        strength += int.Parse(text[i + 1].ToString());
+        sb.Append(text[i]);
+        continue;
+    }
+
+    if (strength > 0)
+    {
+        strength--;
+    }
+    else
+    {
+        sb.Append(text[i]);
+    }
+}
+Console.WriteLine(sb);
