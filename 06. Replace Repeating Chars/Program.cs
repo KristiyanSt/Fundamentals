7f04e4a51b48538b93edfcdc044c118325f911c4
Kristian <127381299+KristiyanSt@users.noreply.github.com>
2023-03-13 14:57:43 +0200

    Add new task called "06. Replace Repeating Chars"

---
+++ 06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+string text = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 1; i < text.Length; i++)
+{
+
+    if (text[i] != text[i - 1])
+    {
+        sb.Append(text[i - 1]);
+    }
+
+}
+sb.Append(text.Last());
+Console.WriteLine(sb);
